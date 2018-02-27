40e3ae0d022fe3ea03e51fb66054d59530a48e28
Alexander Shcherbakov <Alexander_Shcherbakov@epam.com>
2018-02-27 12:00:35 +0300

    removed prettier

--- client/components/Counter/Counter.js
+++ client/components/Counter/Counter.js
@@ -9,50 +9,50 @@ import styles from './Counter.css';
 
 @CSSModules(styles, { allowMultiple: true })
 class Counter extends Component {
-    static propTypes = {
-        value: PropTypes.number.isRequired,
-        onIncrement: PropTypes.func.isRequired,
-        onDecrement: PropTypes.func.isRequired,
-    };
+  static propTypes = {
+      value: PropTypes.number.isRequired,
+      onIncrement: PropTypes.func.isRequired,
+      onDecrement: PropTypes.func.isRequired,
+  };
 
-    constructor(props) {
-        super(props);
-        this.incrementAsync = this.incrementAsync.bind(this);
-        this.incrementIfOdd = this.incrementIfOdd.bind(this);
-    }
+  constructor(props) {
+      super(props);
+      this.incrementAsync = this.incrementAsync.bind(this);
+      this.incrementIfOdd = this.incrementIfOdd.bind(this);
+  }
 
-    incrementIfOdd() {
-        if (this.props.value % 2 !== 0) {
-            this.props.onIncrement();
-        }
-    }
+  incrementIfOdd() {
+      if (this.props.value % 2 !== 0) {
+          this.props.onIncrement();
+      }
+  }
 
-    incrementAsync() {
-        setTimeout(this.props.onIncrement, 1000);
-    }
+  incrementAsync() {
+      setTimeout(this.props.onIncrement, 1000);
+  }
 
-    render() {
-        const { value, onIncrement, onDecrement } = this.props;
-        return (
-            <Fragment>
-                <p styleName="red">
-                    Clicked: { value } times
-                </p>
+  render() {
+      const { value, onIncrement, onDecrement } = this.props;
+      return (
+          <Fragment>
+              <p styleName="red">
+                  Clicked: { value } times
+              </p>
 
-                <p>
-                    <button onClick={ onIncrement }>+</button>
+              <p>
+                  <button onClick={ onIncrement }>+</button>
 
-                    <button onClick={ onDecrement }>-</button>
-                </p>
+                  <button onClick={ onDecrement }>-</button>
+              </p>
 
-                <p>
-                    <button onClick={ this.incrementIfOdd }>Increment if odd</button>
+              <p>
+                  <button onClick={ this.incrementIfOdd }>Increment if odd</button>
 
-                    <button onClick={ this.incrementAsync }>Increment async</button>
-                </p>
-            </Fragment>
-        );
-    }
+                  <button onClick={ this.incrementAsync }>Increment async</button>
+              </p>
+          </Fragment>
+      );
+  }
 }
 
 export default connect(state => ({ value: state.counter }), {
--- client/store.js
+++ client/store.js
@@ -3,6 +3,5 @@ import counterApp from './reducers';
 
 export const store = createStore(
     counterApp,
-    window.__REDUX_DEVTOOLS_EXTENSION__ &&
-		window.__REDUX_DEVTOOLS_EXTENSION__(),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
 );
