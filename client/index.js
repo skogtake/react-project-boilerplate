95d69a5475adfc18d97b7b2eccd331ff674ccd34
Alexander Shcherbakov <Alexander_Shcherbakov@epam.com>
2018-05-24 13:13:19 +0300

    updeted packages added redux extensions

--- client/index.js
+++ client/index.js
@@ -3,7 +3,7 @@ import { render } from 'react-dom';
 import { Provider } from 'react-redux';
 
 import App from './containers/App';
-import { store } from './store';
+import store from './store';
 
 render(
     <Provider store={ store }>
--- client/store.js
+++ client/store.js
@@ -1,7 +1,11 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from 'redux-devtools-extension';
+
 import counterApp from './reducers';
 
-export const store = createStore(
+const store = createStore(
     counterApp,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+    composeWithDevTools(),
 );
+
+export default store;
