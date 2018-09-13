084d4a7e7317012972710ec894df4630a999a99f
Alexander Shcherbakov <Alexander_Shcherbakov@epam.com>
2018-09-13 16:52:00 +0300

    worked around

---
+++ public/precache-manifest.b095af4742c6811556affeb88f1f4509.js
@@ -0,0 +1,10 @@
+self.__precacheManifest = [
+  {
+    "revision": "82fd0b9c76a689e4c4ad5b768bee7874",
+    "url": "./bundle.js.LICENSE"
+  },
+  {
+    "revision": "3e9144e52c2886958a3d",
+    "url": "./bundle.js"
+  }
+];
--- public/sw.js
+++ public/sw.js
@@ -11,10 +11,10 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.4.1/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.5.0/workbox-sw.js");
 
 importScripts(
-  "precache-manifest.b043cbc9bb71c34c35507d62588eda39.js"
+  "precache-manifest.b095af4742c6811556affeb88f1f4509.js"
 );
 
 workbox.skipWaiting();
